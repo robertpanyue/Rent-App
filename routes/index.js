54a1b1bd8e10c76d608b676e2c0c19a197570ad3
Yue Pan <robertpanyue@gmail.com>
2019-04-29 22:04:27 -0400

    update and merge

--- routes/index.js
+++ routes/index.js
@@ -5,25 +5,24 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	try {
-		res.render('pages/mainPage', { title: 'Main Page' });
-	} catch (e) {
-		res.status(400).render('pages/error', { errorMessage: 'Main page Error', title: 'Error' });
-	}
+    try {
+        res.render('pages/mainPage', { title: 'Main Page' });
+    } catch (e) {
+        res.status(400).render('pages/error', { errorMessage: 'Main page Error', title: 'Error' });
+    }
 });
 
 const constructorMethod = (app) => {
-	app.get('/', router);
-	app.get('/register', registerRoutes);
-	app.post('/register', registerRoutes);
-	//app.use('/login', loginRoutes);
-	app.get('/main', mainPageSearchRoutes);
-	app.use('*', (req, res) => {
-		res.status(404).json({
-			error: 'Invalid Route',
-			route: req.originalUrl,
-			method: req.method
-		});
-	});
+    app.get('/', router);
+    app.use('/register', registerRoutes);
+    //app.use('/login', loginRoutes);
+    app.use('/main', mainPageSearchRoutes);
+    app.use('*', (req, res) => {
+        res.status(404).json({
+            error: 'Invalid Route',
+            route: req.originalUrl,
+            method: req.method
+        });
+    });
 };
 module.exports = constructorMethod;
--- routes/register.js
+++ routes/register.js
@@ -3,28 +3,36 @@ const router = express.Router();
 const firebase = require('firebase');
 const firebaseAuth = require('firebase/auth');
 
-router.get('/register', async(req, res) => {
-    console.log('get register');
-    try {
-        res.render('pages/register');
-    } catch (error) {
-        res.status(400).render('pages/error', {
-            errorMessage: 'Register Error',
-            title: 'Error'
-        });
-    }
+router.get('/', async (req, res) => {
+	console.log('get register');
+	try {
+		res.render('pages/register');
+	} catch (error) {
+		res.status(400).render('pages/error', {
+			errorMessage: 'Register GET Error',
+			title: 'Error'
+		});
+	}
 });
 
-router.post('/register', async(req, res) => {
-    try {
-
-        res.redirect('/main');
-    } catch (error) {
-        res.status(400).render('pages/error', {
-            errorMessage: 'Register Post Error',
-            title: 'Error'
-        });
-    }
+router.post('/', async (req, res) => {
+	try {
+		console.log(`registered`);
+		console.log(req.body.email);
+		console.log(req.body.password);
+		// firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.password).catch(function(error) {
+		//     res.status(400).render('pages/error', {
+		//         errorMessage: 'Register Post Error' + `${error}`,
+		//         title: 'Error'
+		//     });
+		// });
+		res.redirect('/main');
+	} catch (error) {
+		res.status(400).render('pages/error', {
+			errorMessage: 'Register Post Error!' + `${error}`,
+			title: 'Error'
+		});
+	}
 });
 
 module.exports = router;
