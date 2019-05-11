b225164e068d585441f966e122d71ed9ce257d74
Yue Pan <robertpanyue@gmail.com>
2019-05-11 18:52:44 -0400

    finish search

--- public/javascript/searchAddResult.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	const item = window.location.pathname;
-	var splittable = item.split('/');
-	string1 = splittable[0];
-	string2 = splittable[1];
-	$.ajax = {
-		url: `/search/result/${string2}`
-	};
-});
--- routes/search.js
+++ routes/search.js
@@ -2,15 +2,50 @@ const express = require('express');
 const router = express.Router();
 const db = require('../data/dbCollections');
 const items = db.itemPosts;
-router.get('/:item', async (req, res) => {
+router.get('/listings/:item', async (req, res) => {
+	try {
+		if (req.session && req.session.user) {
+			const keyWord = req.params.item;
+			const itemCollection = await items();
+			const searchType = req.body.selectSearch;
+			//get the result of the key word from the datbase
+			const result = await itemCollection.find({ $text: { $search: keyWord } }).toArray();
+			const returnArray = [];
+
+			for (let i = 0; i < result.length; i++) {
+				if (result[i].requested == 'Listed') {
+					returnArray.push(result[i]);
+				}
+			}
+
+			res.render('pages/searchResult', { title: keyWord, type: 'Listings', resultList: returnArray });
+		} else {
+			res.render('pages/error', { errorMessage: 'You do not have authentication', title: '403' });
+		}
+	} catch (error) {
+		res.status(400).render('pages/error', {
+			errorMessage: 'Search Error' + `${error}`,
+			title: 'Error'
+		});
+	}
+});
+
+router.get('/requests/:item', async (req, res) => {
 	try {
 		if (req.session && req.session.user) {
 			const keyWord = req.params.item;
 			const itemCollection = await items();
 			//get the result of the key word from the datbase
-			const result = await itemCollection.find({ $text: { $search: keyWord } });
-			console.log(result);
-			res.render('pages/searchResult', { title: keyWord, type: 'Listed', resultList: result });
+			const result = await itemCollection.find({ $text: { $search: keyWord } }).toArray();
+			const returnArray = [];
+
+			for (let i = 0; i < result.length; i++) {
+				if (result[i].requested == 'Requested') {
+					returnArray.push(result[i]);
+				}
+			}
+
+			res.render('pages/searchResult', { title: keyWord, type: 'Requests', resultList: returnArray });
 		} else {
 			res.render('pages/error', { errorMessage: 'You do not have authentication', title: '403' });
 		}
@@ -25,7 +60,12 @@ router.get('/:item', async (req, res) => {
 router.post('/', async (req, res) => {
 	try {
 		if (req.session && req.session.user) {
-			res.redirect(`/search/${req.body.searchContent}`);
+			const searchType = req.body.selectSearch;
+			if (searchType == 'Listings') {
+				res.redirect(`/search/listings/${req.body.searchContent}`);
+			} else {
+				res.redirect(`/search/requests/${req.body.searchContent}`);
+			}
 		} else {
 			res.render('pages/error', { errorMessage: 'You do not have authentication', title: '403' });
 		}
