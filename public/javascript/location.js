f9cf3a91aa163e8baf8887f4651711f91a1ab71d
Yue Pan <robertpanyue@gmail.com>
2019-05-12 16:42:47 -0400

    add geolocation button in the mainSearch page

--- public/javascript/location.js
+++ public/javascript/location.js
@@ -6,9 +6,18 @@ function geoFindMe() {
 		const lat = position.coords.latitude;
 		const long = position.coords.longitude;
 		var point = new google.maps.LatLng(lat, long);
+		var city = '';
 		new google.maps.Geocoder().geocode({ latLng: point }, function(res, status) {
+			var component = res[0].address_components;
+			for (let i = 0; i < component.length; i++) {
+				for (let j = 0; j < component[i].types.length; j++) {
+					if (component[i].types[j] == 'locality') {
+						city = component[i].long_name;
+					}
+				}
+			}
 			var zip = res[0].formatted_address.match(/,\s\w{2}\s(\d{5})/);
-			button.textContent = `Zip: ${zip[1]}`;
+			button.textContent = `City: ${city} Zip: ${zip[1]}`;
 		});
 	}
 
